8a57b01cd5d57f2d1f806bed688ccf1890b4f6d2
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 15:58:49 +0530

    Add. Logical operators example

---
+++ logical-operators/ageChecker.js
@@ -0,0 +1,9 @@
+let age = 22;
+
+if (age >= 18) {
+    console.log("You are a major!!");
+} else if (age >= 14 && age < 18) {
+    console.log("You are an adult!!");
+} else {
+    console.log("You are a child!!");
+}
