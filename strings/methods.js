4baa37503bd16ad279bf32c2527aebdfdc3dbb99
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 12:44:38 +0530

    Add. Few more methods example of strings

--- strings/methods.js
+++ strings/methods.js
@@ -8,3 +8,9 @@ console.log("Upper case of avenger:", upperCaseOfAvenger);
 
 let indexOfN = avenger.indexOf("n");
 console.log("Index of N in avenger:", indexOfN);
+
+let lastIndexOfN = avenger.lastIndexOf("n");
+console.log("Last index of N is avenger:", lastIndexOfN);
+
+let slicedAvenger = avenger.slice(2, 5);
+console.log("Sliced avenger:", slicedAvenger);
