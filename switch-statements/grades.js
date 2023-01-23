f5aa882e5ab01bacff3582e18ed4033bcbd0f2de
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 16:17:51 +0530

    Add. Switch statement example

---
+++ switch-statements/grades.js
@@ -0,0 +1,22 @@
+let grade = 'C';
+
+switch (grade) {
+    case 'A':
+        console.log("Congratulations of your A grade!!");
+        break;
+    case 'B':
+        console.log("Congratulations of your B grade!!");
+        break;
+    case 'C':
+        console.log("Congratulations of your C grade!!");
+        break;
+    case 'D':
+        console.log("Congratulations of your D grade!!");
+        break;
+    case 'E':
+        console.log("Congratulations of your E grade!!");
+        break;
+    default:
+        console.log("Better luck next time!!");
+        break;
+}
