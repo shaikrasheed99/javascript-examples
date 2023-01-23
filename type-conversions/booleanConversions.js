64be21d264bb9613456f61b6a24c0642d29d9ac4
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 14:21:44 +0530

    Add. Boolean type conversions example

---
+++ type-conversions/booleanConversions.js
@@ -0,0 +1,15 @@
+let ten = 10;
+let tenInBoolean = Boolean(ten);
+console.log(tenInBoolean);
+
+let zero = 0;
+let zeroInBoolean = Boolean(zero);
+console.log(zeroInBoolean);
+
+let avenger = "ironman";
+let avengerInBoolean = Boolean(avenger);
+console.log(avengerInBoolean);
+
+let emptyString = "";
+let emptyStringInBoolean = Boolean(emptyString);
+console.log(emptyStringInBoolean);
