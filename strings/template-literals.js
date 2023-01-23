3bf6a54db43ad0ce9368b45b935fb51cf321c651
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 12:32:00 +0530

    Add. Template literals example of strings

---
+++ strings/template-literals.js
@@ -0,0 +1,19 @@
+let firstName = "Ratan";
+let lastName = "Tata";
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let stringWithQuote = `He's often called "Ironman"`
+console.log(stringWithQuote);
+
+let multilineString = 
+`He's 
+often 
+called 
+"Ironman"`;
+console.log(multilineString);
+
+let radius = 10;
+const PI = 3.14;
+let areaOfCircle = `Area of circle: ${PI * (radius**2)}`;
+console.log(areaOfCircle);
