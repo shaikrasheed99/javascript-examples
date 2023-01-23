802fd1da50d2fb48a5250cf3ef175bc38d487d4f
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 11:39:32 +0530

    Add. Methods example of strings

---
+++ strings/methods.js
@@ -0,0 +1,10 @@
+let avenger = "Ironman";
+let lowerCaseOfAvenger = avenger.toLowerCase();
+let upperCaseOfAvenger = avenger.toUpperCase();
+
+console.log(avenger);
+console.log("Lower case of avenger:", lowerCaseOfAvenger);
+console.log("Upper case of avenger:", upperCaseOfAvenger);
+
+let indexOfN = avenger.indexOf("n");
+console.log("Index of N in avenger:", indexOfN);
