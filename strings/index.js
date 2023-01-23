3d1e8e9a160b53ebc2d3d56e66027cfac2bbc4a5
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 11:32:46 +0530

    Add. Few more examples of strings

--- strings/index.js
+++ strings/index.js
@@ -1,2 +1,6 @@
 let avenger = "Ironman";
 console.log("Value of string is:", avenger);
+
+console.log("Length of avenger is:", avenger.length);
+
+console.log("Third character of avenger is:", avenger[2]);
