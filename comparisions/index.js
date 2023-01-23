af65573d041292f846094652cc9e7300ff1e7fcb
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-23 13:27:02 +0530

    Add. Comparisions example

---
+++ comparisions/index.js
@@ -0,0 +1,20 @@
+let age = 25;
+console.log(age);
+
+let isLessThan = age < 25;
+console.log(isLessThan);
+
+let isLessThanEquals = age <= 25;
+console.log(isLessThanEquals);
+
+let isGreaterThan = age > 25;
+console.log(isGreaterThan);
+
+let isGreaterThanEquals = age >= 25;
+console.log(isGreaterThanEquals);
+
+let isEquals = age == 25;
+console.log(isEquals);
+
+let isNotEquals = age != 30;
+console.log(isNotEquals);
