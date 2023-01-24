f4d7c99f6f0b458ae5e96ee296ebceedcb9e909e
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-24 16:26:23 +0530

    Add. Arrays example

---
+++ arrays/index.js
@@ -0,0 +1,8 @@
+const avengers = ["Ironman", "Thor", "Thanos", 3000];
+console.log(avengers);
+
+avengers.push("Captain")
+console.log(avengers);
+
+const removedAvenger = avengers.pop()
+console.log(removedAvenger);
