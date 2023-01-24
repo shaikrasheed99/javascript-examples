1e9feefeb571697c01cc7fe6cbf53057a39a6280
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-24 16:44:10 +0530

    Add. For each loop example

---
+++ for-each-loop/index.js
@@ -0,0 +1,5 @@
+const avengers = ["Ironman", "Thor", "Thanos", "Captain"];
+
+const displayAvengers = (avenger, index) => console.log(`Index of ${avenger} is ${index}`);
+
+avengers.forEach(displayAvengers)
