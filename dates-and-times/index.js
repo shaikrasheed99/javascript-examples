9a6be0e844347311282b2c3d3d6d9d46c439a2ae
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-02 10:31:07 +0530

    Add. Dates & times example

---
+++ dates-and-times/index.js
@@ -0,0 +1,12 @@
+const today = new Date();
+
+console.log("Year is: ", today.getFullYear());
+console.log("Month is: ", today.getMonth());
+console.log("Day is: ", today.getDay());
+console.log("Date is: ", today.getDate());
+console.log("Time is: ", today.getTime());
+console.log("Hours are: ", today.getHours());
+console.log("Minutes are: ", today.getMinutes());
+console.log("Seconds are: ", today.getSeconds());
+console.log("Date string is: ", today.toDateString());
+console.log("Time string is: ", today.toTimeString());
